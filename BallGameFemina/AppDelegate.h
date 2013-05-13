45fa99c35e4151fca7584e8e6ca6388b501b3213
Femina Ernest <femina@Feminas-MacBook-Air.local>
2013-05-13 21:55:24 +0530

    Initial Commit

---
+++ BallGameFemina/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BallGameFemina
+//
+//  Created by Femina Ernest on 5/13/13.
+//  Copyright (c) 2013 Femina Ernest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BallGameFemina/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BallGameFemina
+//
+//  Created by Femina Ernest on 5/13/13.
+//  Copyright (c) 2013 Femina Ernest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
